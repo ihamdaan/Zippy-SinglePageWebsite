e55800f8ee1eb6bb42806790f558c26ce8975b03
ihamdaan <20CBS1082@cuchd.in>
2021-07-16 13:42:49 +0530

    final website

---
+++ index.js
@@ -0,0 +1,45 @@
+$('.slider-01').slick({
+    
+    arrows: true,
+    centerMode: true,
+    centerPadding: '200px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '300px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '1px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
